47762561c6d2e5fcf464c43ec3e8d70f53d6e551
Chaeros <chpark6851@koreatech.ac.kr>
2023-06-15 13:08:56 +0900

    뉴렉쳐 스프링프레임워크 11강 실습

--- newLectureSpring/src/main/java/spring/di/Program.java
+++ newLectureSpring/src/main/java/spring/di/Program.java
@@ -1,5 +1,11 @@
 package spring.di;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import spring.di.entity.*;
 import spring.di.ui.ExamConsole;
 import spring.di.ui.GridExamConsole;
@@ -7,10 +13,24 @@ import spring.di.ui.InlineExamConsole;
 
 public class Program {
 		public static void main(String[] args) {
-			Exam exam=new NewlecExam();
-			ExamConsole console = new InlineExamConsole(exam);  // exam 객체를 DI 하고있다.
+			//ExamConsole console = new InlineExamConsole(exam);  // exam 객체를 DI 하고있다.
 			//ExamConsole console = new GridExamConsole(exam);
-			console.setExam(exam);
+			//console.setExam(exam);
+			
+			ApplicationContext context = new ClassPathXmlApplicationContext("spring/di/setting.xml");
+			
+			Exam exam= context.getBean(Exam.class);
+			System.out.println(exam.toString());
+			ExamConsole console = (ExamConsole) context.getBean("console");
+			//ExamConsole console = context.getBean(ExamConsole.class);
 			console.print();
+			
+			//List<Exam> exams=new ArrayList<>();
+			//exams.add(new NewlecExam(1,1,1,1));
+			
+			List<Exam> exams = (List<Exam>) context.getBean("exams");
+			
+			for(Exam e:exams)
+				System.out.println(e);
 		}
 }
